f823c3fb0ae84f7c46cf997d6f665901c15a82df
Sokuphumlela Nomila <nombila.s@gmail.com>
2019-09-03 19:51:26 +0200

    Update RoleFactoryTest.java

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/misc/RoleFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/misc/RoleFactoryTest.java
@@ -6,18 +6,10 @@ import static org.junit.Assert.*;
 
 public class RoleFactoryTest {
 
-    //TODO: implement method body ONLY!
     @Test
     public void buildRole() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not yet supported.");]
-         * 3. Test the RoleFactory class
-         * 4. Assert that the id is generated.
-         */
+     Role role=RoleFactory.buildRole("Sd");
+     Assert.assertNotNull(role);
+     System.out.println(role.toString());
     }
 }
