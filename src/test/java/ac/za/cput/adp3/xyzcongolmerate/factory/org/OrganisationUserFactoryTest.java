c0db9aa7255d28e3b22e6c701ae465a6409774ed
Sokuphumlela Nomila <nombila.s@gmail.com>
2019-09-03 22:38:08 +0200

    Update OrganisationUserFactoryTest.java

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/org/OrganisationUserFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/org/OrganisationUserFactoryTest.java
@@ -6,18 +6,9 @@ import static org.junit.Assert.*;
 
 public class OrganisationUserFactoryTest {
 
-    //TODO: implement method body ONLY!
     @Test
     public void buildOrganisationUser() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 1. Remove line [throw new UnsupportedOperationException("Not yet supported.");]
-         * 2. Test the OrganisationUserFactory class
-         * 3. Assert that the an object is created.
-         */
+        OrganisationUser organisationUse=OrganisationUserFactory.buildOrganisationUser("23", "lun@gh.com");
+        Assert.assertNotNull(organisationUse);
     }
 }
